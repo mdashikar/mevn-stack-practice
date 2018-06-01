6720b42dbb5b378a308eabd69daec0619fb55506
Md Ashiqur Rahman <md.ashik.ar@gmail.com>
2018-06-02 04:44:59 +0600

    First Commit

---
+++ config/secret.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //    database : 'mongodb://first:123456@ds259305.mlab.com:59305/synergy_proposal_submit',
+    database: 'mongodb://root:abc123@ds255258.mlab.com:55258/practice',
+    secret: '$43@#asb)9!~asd&^%'
+};
